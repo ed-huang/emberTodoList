8a2dafaa68bafded1ccc5b6e6d55732864f17bb9
Ed <ed@edhuang.com>
2014-08-02 15:10:49 +0800

    updated todo list

--- js/router.js
+++ js/router.js
@@ -1,8 +1,43 @@
 Todos.Router.map(function() {
-	this.resource('todos', { path: '/' });
+	console.log('map');
+	this.resource('todos', { path: '/' }, function() {
+		this.route('active');
+		this.route('completed');
+		//stuff will go here
+	});
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+	model: function() {
+		return this.store.filter('todo', function(todo) {
+			return todo.get('isCompleted');
+		});
+	},
+
+	renderTemplate: function(controller) {
+		this.render('todos/index', {controller: controller});
+	}
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+	model: function() {
+		return this.store.filter('todo', function(todo) {
+			return !todo.get('isCompleted');
+		});
+	},
+
+	renderTemplate: function(controller) {
+		this.render('todos/index', {controller: controller})
+	}
 });
 
 Todos.TodosRoute = Ember.Route.extend({
+	model: function() {
+		return this.store.find('todo');
+	}
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
 	model: function() {
 		return this.store.find('todo');
 	}
